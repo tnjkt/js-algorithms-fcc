7046608b96caec263eb19c81a65e75a0b799775d
tnjkt <naujokaitetoma@gmail.com>
2021-09-14 15:36:50 +0300

    sorted-union

--- sorted-union.js
+++ sorted-union.js
@@ -1,18 +1,25 @@
 function uniteUnique(...arrs) {
   console.log(arrs);
-  const newArr = [];
 
   const allArrs = arrs.reduce(
     (arrOfArrs, currArr) => arrOfArrs.concat(currArr),
     []
   );
 
-  for (let i = 0; i < allArrs.length; i++) {
-    const num = allArrs[i];
-    if (newArr.indexOf(num) == -1) {
-      newArr.push(num);
+  //    const newArr=[];
+  //    for (let i = 0; i < allArrs.length; i++) {
+  //     const num = allArrs[i];
+  //     if (newArr.indexOf(num) == -1) {
+  //       newArr.push(num);
+  //     }
+  //   }
+
+  const newArr = allArrs.reduce((arr, num) => {
+    if (arr.indexOf(num) == -1) {
+      return [...arr, num];
     }
-  }
+    return arr;
+  }, []);
   return newArr;
 }
 
